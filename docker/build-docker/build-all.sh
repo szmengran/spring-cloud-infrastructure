d552698684b7db09e986560f337500a4e37bd1cf
szmengran <android_li@sina.cn>
2018-09-16 20:25:50 +0800

    update build file

--- docker/build-docker/build-all.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sh ./build-docker/build-config.sh
-sh ./build-docker/build-eureka.sh
-sh ./build-docker/build-gateway.sh
-sh ./build-docker/build-oauth.sh
---
+++ docker/build/all.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sh ./build/config.sh
+sh ./build/eureka.sh
+sh ./build/gateway.sh
+sh ./build/oauth.sh
