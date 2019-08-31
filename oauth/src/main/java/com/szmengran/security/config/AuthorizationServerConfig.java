24797b8a38fae33699e9cd794e7ffbdd3ce240d7
limaoyuan <android_li@sina.cn>
2019-08-31 15:15:49 +0800

    spring boot version update

--- oauth/src/main/java/com/szmengran/security/config/AuthorizationServerConfig.java
+++ oauth/src/main/java/com/szmengran/security/config/AuthorizationServerConfig.java
@@ -1,72 +1,72 @@
-package com.szmengran.security.config;
-
-
-import javax.annotation.Resource;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
-
-import com.alibaba.druid.pool.DruidDataSource;
-import com.szmengran.security.service.UserService;
-
-@Configuration
-@EnableAuthorizationServer
-public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
-    @Autowired
-    private AuthenticationManager authenticationManager;
-    @Autowired
-    private RedisConnectionFactory connectionFactory;
-    @Resource
-    private DruidDataSource writeDataSource;
-    @Bean
-    public RedisTokenStore tokenStore() {
-        return new RedisTokenStore(connectionFactory);
-    }
-
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        return new UserService();
-    }
-
-    @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        try {
-            endpoints
-                .authenticationManager(authenticationManager)
-                .userDetailsService(userDetailsService())//若无，refresh_token会有UserDetailsService is required错误
-                .tokenStore(tokenStore());
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-        security
-            .tokenKeyAccess("permitAll()")
-            .checkTokenAccess("isAuthenticated()").passwordEncoder(passwordEncoder());
-    }
-   
-    @Override
-    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients
-            .jdbc(this.writeDataSource)
-            .passwordEncoder(passwordEncoder());
-    }
-}
+//package com.szmengran.security.config;
+//
+//
+//import javax.annotation.Resource;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.redis.connection.RedisConnectionFactory;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+//import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+//import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+//import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+//import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
+//
+//import com.alibaba.druid.pool.DruidDataSource;
+//import com.szmengran.security.service.UserService;
+//
+//@Configuration
+//@EnableAuthorizationServer
+//public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+//    @Autowired
+//    private AuthenticationManager authenticationManager;
+//    @Autowired
+//    private RedisConnectionFactory connectionFactory;
+//    @Resource
+//    private DruidDataSource writeDataSource;
+//    @Bean
+//    public RedisTokenStore tokenStore() {
+//        return new RedisTokenStore(connectionFactory);
+//    }
+//
+//    @Bean
+//    public BCryptPasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        return new UserService();
+//    }
+//
+//    @Override
+//    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+//        try {
+//            endpoints
+//                .authenticationManager(authenticationManager)
+//                .userDetailsService(userDetailsService())//若无，refresh_token会有UserDetailsService is required错误
+//                .tokenStore(tokenStore());
+//        } catch(Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    @Override
+//    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+//        security
+//            .tokenKeyAccess("permitAll()")
+//            .checkTokenAccess("isAuthenticated()").passwordEncoder(passwordEncoder());
+//    }
+//   
+//    @Override
+//    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+//        clients
+//            .jdbc(this.writeDataSource)
+//            .passwordEncoder(passwordEncoder());
+//    }
+//}
--- oauth/src/main/java/com/szmengran/security/config/SecurityConfig.java
+++ oauth/src/main/java/com/szmengran/security/config/SecurityConfig.java
@@ -1,17 +1,19 @@
 package com.szmengran.security.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
-@EnableWebSecurity
+@EnableOAuth2Sso
+@EnableAutoConfiguration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     
     @Autowired
